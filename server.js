67f9fed3c70df9aaa200405aba3ad47307026236
Jpeyton <jpwalk319@gmail.com>
2021-04-13 12:18:50 -0600

    amended link hrefs so they went to the right pages

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const { static } = require('express');
-const express= require('express');
+const express = require('express');
 const dotenv = require("dotenv");
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
 app.use(express.static('public'));
+
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}/`);
+});
