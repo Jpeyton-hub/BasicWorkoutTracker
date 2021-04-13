876bcc83f6d0ce61c64f756506bd1bfa944c46ea
Jpeyton <jpwalk319@gmail.com>
2021-04-13 12:34:21 -0600

    added the accurate workout model for the database and set up index model file, added requirements to the api routes to use mongoose and our db

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+let mongoose = require("mongoose");
+let db = require("../models");
 
 
 router.post("/api/workouts", (req, res) => {
 
 });
+
+module.exports = router;
